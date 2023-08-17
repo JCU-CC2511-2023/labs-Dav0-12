e98fe28574e59efe85f65bc1e4606eb6dec8e3a8
Dav0-12 <140329260+Dav0-12@users.noreply.github.com>
2023-08-17 16:00:25 +1000

    Some commenting changes

--- Lab4/main.c
+++ Lab4/main.c
@@ -4,45 +4,51 @@
  * Lab4
  * ***********************************************************/
 
+// INCLUDE BOOLEAN FUNCTIONALITY
 #include <stdbool.h>
 
+// DEFINE REGISTERS AND FUNCTIONS
 #define SIO_BASE 0xd0000000
-#define LED_PIN 25
+#define LED_PIN 25 // GPIO PIN 25
 
-#define REG_PAD_CONTROL 0x4001c068 //Input enable and output disable registor. Write 1 to pin 7, Write 0 to pin 6
-#define VAL_IE_OD 0x40 //00100 0000
+#define REG_PAD_CONTROL 0x4001c068 // INPUT ENABLE AND OUTPUT DISABLE REGISTER. WRITE 1 TO PIN 7, WRITE 0 TO PIN 6.
+#define VAL_IE_OD (1 << 6)         // 00100 0000
 
-#define REG_GPIO25_CTRL 0x400140cc //Register for the pad control
-#define FUNCTION_5_SET 5 //Set SIO function
+#define REG_GPIO25_CTRL 0x400140cc // REGISTER FOR THE PAD CONTROL
+#define FUNCTION_5_SET 5           // SIO FUNCTION INDENTIFIER
 
-#define REG_GPIO_SET (SIO_BASE+0x024)
-
-#define REG_GPIO_OE_SET (SIO_BASE+0x024)
-#define REG_GPIO_OUT_SET (SIO_BASE+0x014)
-#define REG_GPIO_OUT_CLR (SIO_BASE+0x018)
+#define REG_GPIO_OE_SET (SIO_BASE + 0x024)  // GPIO PIN DIRECTION REGISTER
+#define REG_GPIO_OUT_SET (SIO_BASE + 0x014) // OUTPUT SET REGISTER. TURNING ON PIN.
+#define REG_GPIO_OUT_CLR (SIO_BASE + 0x018) // OUTPUT CLEAR REGISTER. TURNING OFF/CLEAR PIN.
 #define TOGGLE_GPIO25 (1 << LED_PIN)
 
-#define CONTENT_OF(addr) (*(volatile uint_32t*)addr)
+// CREATE MACRO FUNCTIONS
+#define CONTENT_OF(addr) (*(volatile uint_32t *)addr) // ACCESSES THE CONTENTS OF A REGISTER ADDRESS
 
+// CREATE VARIABLE TYPES
 typedef unsigned long uint_32t;
-//Setup wait function
-void wait(uint_32t wait_length){
-  for(uint_32t i = 0; i<wait_length; i++) {
-    __asm volatile ("nop");
+
+// SETUP WAIT FUNCTION
+void wait()
+{
+  for (uint_32t i = 0; i < 8000000; i++) // ARBITRARY DELAY VALUE
+  { 
+    __asm volatile("nop");
   }
 }
 
-int main(void) {
-  //Initialise
-  CONTENT_OF(REG_PAD_CONTROL) = VAL_IE_OD;          // SETUP PAD CONTROL. INPUT AND OUTPUT ENABLED  
-  CONTENT_OF(REG_GPIO25_CTRL) = FUNCTION_5_SET;     // SETS THE SIO FUNCTION
-  CONTENT_OF(REG_GPIO_OE_SET) = TOGGLE_GPIO25;      // SETS THE PIN AS OUTPUT
+int main(void)
+{
+  // INITIALISE
+  CONTENT_OF(REG_PAD_CONTROL) = VAL_IE_OD;      // SETUP PAD CONTROL. INPUT AND OUTPUT ENABLED
+  CONTENT_OF(REG_GPIO25_CTRL) = FUNCTION_5_SET; // SETS THE SIO FUNCTION
+  CONTENT_OF(REG_GPIO_OE_SET) = TOGGLE_GPIO25;  // SETS THE PIN AS OUTPUT
 
-  
-  while (true) {
-    CONTENT_OF(REG_GPIO_OUT_SET) = TOGGLE_GPIO25;
-    wait(8000000);
-    CONTENT_OF(REG_GPIO_OUT_CLR) = TOGGLE_GPIO25;
-    wait(8000000);
+  while (true)
+  {
+    CONTENT_OF(REG_GPIO_OUT_SET) = TOGGLE_GPIO25; // SETS THE GPIO25 PIN TO ON
+    wait();
+    CONTENT_OF(REG_GPIO_OUT_CLR) = TOGGLE_GPIO25; // SETS THE GPIO25 PIN TO OFF
+    wait();
   }
 }
---
+++ Lab4_Extension_Morsecode/main.c
@@ -0,0 +1,21 @@
+/**************************************************************
+ * main.c
+ * rev 1.0 17-Aug-2023 dghun
+ * Lab4_Extension_Morsecode
+ * ***********************************************************/
+
+#include "pico/stdlib.h"
+#include <stdbool.h>
+#include "pico/stdio_usb.h"
+
+int main(void) {
+  //Initialise USB connection
+  stdio_usb_init();
+
+  //
+
+
+  while (true) {
+    
+  }
+}
